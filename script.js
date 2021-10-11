4510ebccba4a4e2377551f793c9ce43008734002
benckx <benoit.vleminckx@restore.eu>
2021-10-11 11:07:11 +0200

    more dynamic render

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
         let a = aTags[i]
         let id = a.id
         if (id != null && id !== "") {
-            a.href = "https://creator.nightcafe.studio/creation/" + id
+            a.href = `https://creator.nightcafe.studio/creation/${id}`
             let img = document.createElement("img");
             img.src = `https://ik.imagekit.io/nightcafe/jobs/${id}/${id}.jpg?tr=w-1600,c-at_max`
             a.append(img)
