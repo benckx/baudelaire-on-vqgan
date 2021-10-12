fb04128524fd22730338930c9de61fbc53c97b83
benckx <benoit.vleminckx@restore.eu>
2021-10-12 16:01:20 +0200

    ref to local images

--- script.js
+++ script.js
@@ -5,10 +5,10 @@
         let a = aTags[i]
         let id = a.id
         if (id != null && id !== "") {
-            // a.href = `https://creator.nightcafe.studio/creation/${id}`
-            a.href = `img/${id}`
+            a.href = `https://creator.nightcafe.studio/creation/${id}`
             let img = document.createElement("img");
-            img.src = `https://ik.imagekit.io/nightcafe/jobs/${id}/${id}.jpg?tr=w-1600,c-at_max`
+            // img.src = `https://ik.imagekit.io/nightcafe/jobs/${id}/${id}.jpg?tr=w-1600,c-at_max`
+            img.src = `img/${id}.jpg`
             a.append(img)
         }
     }
